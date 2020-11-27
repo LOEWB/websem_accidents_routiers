14cc6815e0dee50923b86b8597e7e0ebab0a34af
Benny <benoitloew@yahoo.fr>
2020-11-27 15:51:13 +0100

    feat: - init maven project

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+
+public class Main {
+    public static void main(String[] args) {
+        Model model = ModelFactory.createDefaultModel();
+        System.out.println("ok");
+    }
+}
