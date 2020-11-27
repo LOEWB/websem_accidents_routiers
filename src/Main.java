3cb55f1eaf297267befdc0115499f43048881032
Benny <benoitloew@yahoo.fr>
2020-11-27 15:56:17 +0100

    feat: - created dev branch

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import org.apache.jena.rdf.model.ModelFactory;
 public class Main {
     public static void main(String[] args) {
         Model model = ModelFactory.createDefaultModel();
-        System.out.println("ok");
+        System.out.println("dev");
     }
 }
